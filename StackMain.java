bf04da85daa769ca21ef9be494bc28a585b54a86
rishika251 <rishikasadineni@gmail.com>
2022-11-18 07:56:30 +0530

    Added:ability to create stack

---
+++ StackMain.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.stackandqueue;
+
+public class StackMain {
+	public static void main(String[] args) {
+
+		Stack stack = new Stack();
+		System.out.println("Stack Operation Adding");
+		stack.push(70);
+		stack.display();
+		stack.push(30);
+		stack.display();
+		stack.push(56);
+		stack.display();
+
+	}
+}
+
