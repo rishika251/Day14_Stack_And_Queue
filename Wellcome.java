53a7358a94315cee05bf5d4af0b44feb875792ee
rishika251 <rishikasadineni@gmail.com>
2022-11-18 07:48:34 +0530

    Added:wellcome

---
+++ Wellcome.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.stackandqueue;
+
+public class Wellcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Wellcome to stack and queue problems");
+	}
+
+}
